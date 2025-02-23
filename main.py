0c694823dcd2abddfbc9c67390c8891351d3ba1b
Sanjin Jareb <Sanjin.jareb@hotmail.com>
2025-02-23 11:07:09 +0100

    Updated product prices and quantity

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from project_manager import ProductManager
 
 manager = ProductManager()
 
-manager.add_product(Product("TV", 700, 10))
-manager.add_product(Product("iPhone", 1200, 70))
-manager.add_product(Product("Laptop", 900, 50))
+manager.add_product(Product("TV", 700, 20))
+manager.add_product(Product("iPhone", 1100, 70))
+manager.add_product(Product("Laptop", 1000, 30))
 
-print("Products: ")
-manager.display_products()
-
-print("Value of all products: ")
-manager.total_value()
 
